51e4d34d31e1162e1a3f9bd8c904fe2452d85736
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-04-02 13:01:20 +0530

    ✨ Formatting

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-async function main(){
+async function main() {
     const apiUrl = "https://api.github.com/users/Chiroyce1/repos";
     const repos = await (await fetch(apiUrl)).json();
     const exceptions = ['Chiroyce1', 'markdown']
     repos.forEach(repo => {
-        if (!repo.fork && !repo.archived && !exceptions.includes(repo.name)){
+        if (!repo.fork && !repo.archived && !exceptions.includes(repo.name)) {
             let card = document.createElement('a');
             let img = document.createElement('img');
             let imgUrl = `https://github-readme-stats.vercel.app/api/pin/?username=Chiroyce1&repo=${repo.name}&show_owner=true&theme=github_dark`;
             img.src = imgUrl;
-            img.setAttribute('class', 'repo')
-            card.href = repo['html_url'];
+            img.classList.add('repo');
+            card.href = repo.html_url;
             card.appendChild(img);
             document.querySelector('.repos').appendChild(card);
         }
